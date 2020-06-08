fffb9a7bd337853f98a42087518517a7f3cb2aa6
Aneesh MV <30946083+amv715@users.noreply.github.com>
2020-06-09 00:01:30 +0530

    Power of 2

--- Day8.cpp
+++ Day8.cpp
@@ -1,12 +1,25 @@
+/*
+
+Given an integer, write a function to determine if it is a power of two.
+
+Example 1:
+
+Input: 1
+Output: true 
+Explanation: 20 = 1
+
+*/
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n <= 0) 
+        if (n <= 0) //if n is negative or 0 it can not be a power of 2
             return false;
-        while (n % 2 == 0)
+        
+        while (n % 2 == 0) //keep dividing n by 2 if n is even 
         {
             n = n/2;
         }
+        //if n now is not 1 then n can not be a power of 2
         if(n == 1)
         {
             return true;
